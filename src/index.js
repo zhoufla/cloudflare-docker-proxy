84887b3926a04f885d03ef84ff19faca17c27c71
周凤林 <zero187735@163.com>
2024-03-12 23:01:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.8300627.xyz": "https://registry-1.docker.io",
+  "quay.8300627.xyz": "https://quay.io",
+  "gcr.8300627.xyz": "https://gcr.io",
+  "k8s-gcr.8300627.xyz": "https://k8s.gcr.io",
+  "k8s.8300627.xyz": "https://registry.k8s.io",
+  "ghcr.8300627.xyz": "https://ghcr.io",
+  "cloudsmith.8300627.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
